bb3cae8d5072da0f728d5a5f256b15fc0010ae42
Ashen Chathuranga <ktauchathuranga@gmail.com>
2026-03-10 21:59:40 +0530

    protect routes

--- routes/web.php
+++ routes/web.php
@@ -5,10 +5,14 @@ use App\Http\Controllers\ChirpController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [ChirpController::class, 'index']);
-Route::post('/chirps', [ChirpController::class, 'store']);
-Route::get('/chirps/{chirp}/edit', [ChirpController::class, 'edit']);
-Route::put('/chirps/{chirp}', [ChirpController::class, 'update']);
-Route::delete('/chirps/{chirp}', [ChirpController::class, 'destroy']);
+ 
+// Protected routes
+Route::middleware('auth')->group(function () {
+    Route::post('/chirps', [ChirpController::class, 'store']);
+    Route::get('/chirps/{chirp}/edit', [ChirpController::class, 'edit']);
+    Route::put('/chirps/{chirp}', [ChirpController::class, 'update']);
+    Route::delete('/chirps/{chirp}', [ChirpController::class, 'destroy']);
+});
 
 // Registration routes
 Route::view('/register', 'auth.register')
